7130ce845a494af43c6e0fdea30111dbbc6e10da
Aiden Chang <aidenchang@Aidens-MacBook-Pro.local>
2021-01-21 00:57:02 +0900

    revised books.py

--- books/books.py
+++ books/books.py
@@ -1,5 +1,6 @@
 '''
 Author: Aiden Chang, Duc Nguyen
+Revised by: Aiden Chang
 Winter 2021, cs257
 Code will contain arg parse commands. See usage.txt for details.
 '''
@@ -8,15 +9,15 @@ import argparse, csv, sys
 
 def get_parsed_args():
   parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter) 
-  #for the newline characters RawTextHelpFormatter
-  #RawTextHelpFormatter inspired by some post in stack overflow
+  '''for the newline characters RawTextHelpFormatter
+  RawTextHelpFormatter inspired by some post in stack overflow'''
   
   parser.add_argument("--title", "-t", help='''
   Example: python3 books.py -t "Mobi"
   Searches for and displays all books containing the string **book title**. The string is case sensitive and all spaces must be included. Must contain quotation marks.
   '''
   , type = str, nargs = 1)
-  #Inspired by pythons argphase module
+  '''Inspired by pythons argphase module'''
 
   parser.add_argument("--author", "-a", help= '''
   Example: python3 books.py -a "Toni"
@@ -35,68 +36,80 @@ def get_parsed_args():
   Searches for and displays all books published between the **start year** and the **end year** (inclusive). The **start year** must be smaller then the **end year**. 
   ''', type = int, nargs = 2  )
 
-  #Get the file of to search in, default is books.csv
+  
   parser.add_argument('--file', '-f', default='books.csv', help='the file to search for books')
-
+  '''Get the file of to search in, default is books.csv'''
   args = parser.parse_args()
   return args
 
+def get_books_matching_title(search_str, search_file):
+  """
+  Get a dictionary of books and their authors based on search string
 
-"""
-Get a dictionary of books and their authors based on search string
+  Parameters:
+    search_str: String to search for(case-sensitive).
+    search_file: The file to search in.
 
-@param search_str string to search for(case-sensitive)
-@param file the file to search in
-@return dictionary of books with their authors
-"""
-def get_titles(search_str, file):
-  with open(file, newline = '') as csvfile:
+  Returns:
+    titles_with_authors: A dictionary of books with their authors.
+  """
+  with open(search_file, newline = '') as csvfile:
     reader = csv.reader(csvfile, delimiter= ',')
 
     titles_with_authors = {}
-    #Check if the book's title has search string
+    
     for row in reader:
+      '''Check if the book's title has search string'''
       if search_str in row[0]:
           titles_with_authors[row[0]] = row[2]
     return titles_with_authors
 
 
-"""
-Formatted the print result of the title query
-@param search_str string to search for(case-sensitive)
-@param file the file to search in
-"""
-def print_titles(search_str, file):
-  print("You search for books with " + search_str + " in its title in " + file)
-  titles_list = get_titles(search_str, file)
+
+def print_titles(search_str, print_file):
+  """
+  Formatted the print result of the title query
+
+  Parameters:
+    search_str: String to search for(case-sensitive).
+    print_file: The file to search in.
+  """
+  print("You search for books with " + search_str + " in its title in " + print_file)
+  titles_list = get_books_matching_title(search_str, print_file)
   if len(titles_list) == 0:
     print("Sorry, there are no matches. Please check the spelling, capitalization, and spacing. Type --help for more information.")
   for book in titles_list:
     print(book + " by " + titles_list[book])
 
-"""
-Get a dictionary of books and their authors based on search string
 
-@param search_str string to search for(case-sensitive)
-@param file the file to search in
-@return dictionary of books with their authors
-"""
-def get_authors(search_str, file):
-  with open(file, newline = '') as csvfile:
-    reader = csv.reader(csvfile, delimiter= ',') # inspired from python csv module
+def get_books_by_author(search_str, search_file):
+  """
+  Get a dictionary of books and their authors based on search string
 
+  Parameters:
+    search_str: String to search for(case-sensitive).
+    search_file: The file to search in.
+
+  Returns:
+    authors_with_titles: A dictionary of books with their authors.
+  """
+  with open(search_file, newline = '') as csvfile:
+    reader = csv.reader(csvfile, delimiter= ',') 
+    '''inspired from python csv module'''
     authors_with_titles = {}
     for row in reader:
       authors_with_years, title = row[2].split(" ("), row[0]
       authors = [authors_with_years[0]]
 
-      #There are some books that have 2 authors
+      
       if len(authors_with_years) > 2:
+        '''There are some books that have 2 authors'''
         second_author = authors_with_years[1].split(' and ')[1]
         authors.append(second_author)
       
-      #Check if the author's name has search string
+      
       for name in authors:
+        '''Check if the author's name has search string'''
         if search_str in name:
           if name not in authors_with_titles:
             authors_with_titles[name] = [title]
@@ -104,14 +117,17 @@ def get_authors(search_str, file):
             authors_with_titles[name].append(title)
     return authors_with_titles
 
-"""
-Formatted the print result of the author query
-@param search_str string to search for(case-sensitive)
-@param file the file to search in
-"""
-def print_authors(search_str, file):
-  print("You search for authors with " + search_str + " in their names in"  + file)
-  authors_list = get_authors(search_str, file)
+
+def print_books_by_author(search_str, print_file):
+  """
+  Formatted the print result of the author query
+
+  Parameters:
+    search_str: String to search for(case-sensitive).
+    print_file: The file to search in.
+  """
+  print("You search for authors with " + search_str + " in their names in"  + print_file)
+  authors_list = get_books_by_author(search_str, print_file)
   if len(authors_list) == 0:
     print("Sorry, there are no matches. Please check the spelling, capitalization, and spacing. Type --help for more information.")
   for name in authors_list:
@@ -119,17 +135,22 @@ def print_authors(search_str, file):
     for book in authors_list[name]:
       print("   " + book)
 
-"""
-Get a dictionary of books and their published based on a range of year
 
-@param start_year the lower bound of the search range
-@param end_year the upper bound of the search range
-@param file the file to search in
-@return dictionary of books with their publised_year
-"""
-def get_published_years(start_year, end_year, file):
+def get_books_by_years(start_year, end_year, search_file):
+  """
+  Get a dictionary of books and their published based on a range of year
+
+  Parameters:
+    start_year: The lower bound of the search range.
+    end_year: The upper bound of the search range.
+    search_file: The file to search in.
+
+  Returns:
+    titles_with_years: A dictionary of books with their publised_year.
+  """
   titles_with_years = {}
-  with open(file, newline = '') as csvfile: # inspired from python csv module
+  with open(search_file, newline = '') as csvfile: 
+    '''inspired from python csv module'''
     reader = csv.reader(csvfile, delimiter= ',')
     for row in reader:
       title, published_year = row[0], int(row[1])
@@ -137,27 +158,32 @@ def get_published_years(start_year, end_year, file):
         titles_with_years[title] = [published_year]
     return titles_with_years
 
-"""
-Formatted the print result of the published year query
-@param start_year the lower bound of the search range
-@param end_year the upper bound of the search range
-@param file the file to search in
-"""
-def print_published_years(start_year, end_year, file):
-  print("You search for books published from " + str(start_year) + " to " + str(end_year) + " in " + file)
-  year_titles_list = get_published_years(start_year, end_year, file)
+
+def print_books_by_years(start_year, end_year, print_file):
+  """
+  Formatted the print result of the published year query
+
+  Parameters:
+    start_year: The lower bound of the search range.
+    end_year: The upper bound of the search range.
+    print_file: The file to search in
+  """
+  print("You search for books published from " + str(start_year) + " to " + str(end_year) + " in " + print_file)
+  year_titles_list = get_books_by_years(start_year, end_year, print_file)
   if len(year_titles_list) == 0:
     print("Sorry, there are no matches. Please check the spelling, capitalization, and spacing. Type --help for more information.")
   for book in year_titles_list:
     print(book, year_titles_list[book])
 
-"""
-Get a dictionary of books based on multiple characteristic
 
-@param file the file to search in
-"""
-def get_multi_search(file):
-  with open(file, newline = '') as csvfile:
+def get_multi_search(search_file):
+  """
+  Get a dictionary of books based on multiple characteristic
+
+  Parameters:
+    search_file: The file to search in.
+  """
+  with open(search_file, newline = '') as csvfile:
     reader = csv.reader(csvfile, delimiter= ',')
     data_set = []
     for row in reader:
@@ -170,9 +196,11 @@ def get_multi_search(file):
       sys.exit()
     elif title == '':
       pass
-    #Pop the books that does not have search string in its title
+    
     for book in data_set:
-      data_set[:] = [x for x in data_set if title in x[0]] #inspired from some post in stackoverflow 
+      '''Pop the books that does not have search string in its title'''
+      data_set[:] = [x for x in data_set if title in x[0]] 
+      '''inspired from some post in stackoverflow '''
 
     author = input("What is the author of the book?\n")
     if author == "_exit":
@@ -180,11 +208,11 @@ def get_multi_search(file):
     elif author == '':
       pass
     else:
-      #Pop the books that does not have search string in its author's name
+      '''Pop the books that does not have search string in its author's name'''
       for book in data_set:
         authors_with_years = book[2].split(" (")
         authors = [authors_with_years[0]]
-        #There are some books that have 2 authors
+        '''There are some books that have 2 authors'''
         if len(authors_with_years) > 2:
           authors.append(authors_with_years[1].split(' and ')[1])
 
@@ -198,12 +226,13 @@ def get_multi_search(file):
     elif start_year == '':
       pass
     else:
-      #Pop the books that published earlier than starting year
+      '''Pop the books that published earlier than starting year'''
       try:
         for values in data_set:
           data_set[:] = [x for x in data_set if int(start_year) <= int(x[1])] 
-      #Return error if the input is not integer
+      
       except ValueError:
+        '''Return error if the input is not integer'''
         print("Wrong input type. This section will be passed")  
 
     end_year = input("Type in the upper bound of the published year.\n")
@@ -213,26 +242,30 @@ def get_multi_search(file):
       pass
 
     else:
-      #Pop the books that published later than ending year
+      '''Pop the books that published later than ending year'''
       try:
         for values in data_set:
           data_set[:] = [x for x in data_set if int(end_year) >= int(x[1])]
-      #Return error if the input is not integer
+          
       except ValueError:
+        '''Return error if the input is not integer'''
         print("Wrong input type. This section will be passed")  
     
-    #Return nothing if the users skip all
+    
     if ((title == '') and (author == '') and (start_year == '') and (end_year == '')):
+      '''Return nothing if the users skip all'''
       data_set = {}
     return data_set
 
-"""
-Formatted the print result of the multisearch query
 
-@param file the file to search in
-"""
-def print_multi_search(file):
-  books_list = get_multi_search(file)
+def print_multi_search(print_file):
+  """
+  Formatted the print result of the multisearch query
+
+  Parameters:
+    print_file: The file to search in.
+  """
+  books_list = get_multi_search(print_file)
   if len(books_list) == 0:
     print("Sorry, there are no matches. Please check the spelling, capitalization, and spacing. Type --help for more information.")
   for book in books_list:
@@ -240,24 +273,31 @@ def print_multi_search(file):
 
 def main():
   parsed_args = get_parsed_args()
-  file = parsed_args.file
+  aParser = parsed_args.file
 
-  #Warn the user to use the right feature
-  if (parsed_args.title != None and parsed_args.author != None) or (parsed_args.title != None and parsed_args.year != None) or (parsed_args.author != None and parsed_args.year != None):
+  
+  if (parsed_args.title != None and parsed_args.author != None) or \
+    (parsed_args.title != None and parsed_args.year != None) or \
+      (parsed_args.author != None and parsed_args.year != None):
+    '''Warn the user to use the right feature'''
     print("There are too many arguments are being passed. Please use the multisearch feature.")
   
   elif (parsed_args.title != None):
-    print_titles(parsed_args.title[0], file)
+    print_titles(parsed_args.title[0], aParser)
   elif (parsed_args.author != None):
-    print_authors(parsed_args.author[0], file)
+    print_books_by_author(parsed_args.author[0], aParser)
   elif (parsed_args.year != None):
     if (parsed_args.year[0] > parsed_args.year[1]):
       print("The range of year you enter is not valid")
     else:
-      print_published_years(parsed_args.year[0], parsed_args.year[1], file)
+      print_books_by_years(parsed_args.year[0], parsed_args.year[1], aParser)
   else:
-    print_multi_search(file)
+    print_multi_search(aParser)
 
 if __name__ == '__main__':
 	main()
  
+'''
+single purpose functions
+Error messages should go to sys.stderr, not sys.stdout
+'''
