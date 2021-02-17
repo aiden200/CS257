1a88edce013522ca6af89955c1f4bba25b8e080a
Aiden Chang <aidenchang@Aidens-MacBook-Pro.local>
2021-02-18 03:48:38 +0900

    updatded python testing

--- webapp/tests/api-tests.py
+++ webapp/tests/api-tests.py
@@ -8,9 +8,25 @@
 import #whatever the covid api program will look like
 import unittest
 
-class PrimeCheckerTester(unittest.TestCase):
+class ApiEndpointTester(unittest.TestCase):
     def setUp(self):
-
+        self.api_endpoint = #not sure what to put here yet, we don't know yet right? since we don't have the api code
     def tearDown(self):
         pass
 
+    '''
+    testing for api endpoints
+    '''
+    
+    #testing REQUEST: /total_cases?region_contains={state_keyword}
+    def test_cases(self):
+        url = something/total_cases
+        self.assertTrue(self.api_endpoint.get_total_cases(url), 0)
+        self.assertIsNotNone(self.api_endpoint.get_total_cases(url))
+        url = something/total_cases
+        
+
+    #testing REQUEST: /total_vaccinations?region_contains={state_keyword}
+
+
+    #testing REQUEST: /cases_by_date?region_contains={state}
