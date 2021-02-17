e57de2efea353f4e021ed11b7b31c48a4adf5b7a
Aiden Chang <aidenchang@Aidens-MacBook-Pro.local>
2021-02-18 02:32:55 +0900

    added python testing code

---
+++ webapp/tests/api-tests.py
@@ -0,0 +1,16 @@
+'''
+   api-tests.py
+   Aiden Chang, 18 Feb 2021
+   Testing api for covid website project 
+   CS257 winter2021
+'''
+
+import #whatever the covid api program will look like
+import unittest
+
+class PrimeCheckerTester(unittest.TestCase):
+    def setUp(self):
+
+    def tearDown(self):
+        pass
+
